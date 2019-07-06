ecdc469bded93c54d4af25f7fcbcfece51b4c237
smzht <smzhts1@gmail.com>
2019-07-06 20:17:04 +0900

    F2（編集モードに移行）を割り当てるアプリの追加対応をもとに戻す
    ウインドウが切り替わった最初の文字が二回入力される現象が発生するため

--- config.py
+++ config.py
@@ -1799,25 +1799,13 @@ def configure(keymap):
 
 
     ####################################################################################################
-    ## C-Enter に F2（編集モード移行）を割り当てる（オプション）
+    ## Excel の場合、C-Enter に F2（セル編集モード移行）を割り当てる（オプション）
     ####################################################################################################
-    if 0:
-        F2_target = [["EXCEL.EXE",    "EXCEL*"],
-                     ["explorer.exe", "DirectUIHWND"]]
+    if 1:
+        keymap_excel = keymap.defineWindowKeymap(class_name="EXCEL*")
 
-        def is_F2_target(window):
-            windowProcessName = window.getProcessName()
-            windowClassName   = window.getClassName()
-
-            for processName, className in F2_target:
-                if ((processName is None or fnmatch.fnmatch(windowProcessName, processName)) and
-                    (className is None or fnmatch.fnmatch(windowClassName, className))):
-                    return True
-            return False
-
-        keymap_F2 = keymap.defineWindowKeymap(check_func=is_F2_target)
-
-        define_key(keymap_F2, "C-Enter", reset_search(reset_undo(reset_counter(reset_mark(self_insert_command("F2"))))))
+        # C-Enter 押下で、「セル編集モード」に移行する
+        define_key(keymap_excel, "C-Enter", reset_search(reset_undo(reset_counter(reset_mark(self_insert_command("F2"))))))
 
 
     ####################################################################################################
