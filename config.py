9b44bfa67824bfb477a0c4fb8901d11fd6b20343
smzht <smzhts1@gmail.com>
2019-07-10 21:19:42 +0900

    F2（編集モードに移行）を割り当てる方法を汎用化し、explorer.exe を追加

--- config.py
+++ config.py
@@ -1800,13 +1800,21 @@ def configure(keymap):
 
 
     ####################################################################################################
-    ## Excel の場合、C-Enter に F2（セル編集モード移行）を割り当てる（オプション）
+    ## C-Enter に F2（編集モード移行）を割り当てる（オプション）
     ####################################################################################################
     if 0:
-        keymap_excel = keymap.defineWindowKeymap(class_name="EXCEL*")
+        edit_mode_target = [["EXCEL.EXE",    "EXCEL*"],
+                            ["explorer.exe", "DirectUIHWND"]]
 
-        # C-Enter 押下で、「セル編集モード」に移行する
-        define_key(keymap_excel, "C-Enter", reset_search(reset_undo(reset_counter(reset_mark(self_insert_command("F2"))))))
+        def is_edit_mode_target(window):
+            for processName, className in edit_mode_target:
+                if checkWindow(processName, className, window):
+                    return True
+            return False
+
+        keymap_edit_mode = keymap.defineWindowKeymap(check_func=is_edit_mode_target)
+
+        define_key(keymap_edit_mode, "C-Enter", reset_search(reset_undo(reset_counter(reset_mark(self_insert_command("F2"))))))
 
 
     ####################################################################################################
