2f5f455c2f74c00009ff866e4a07a7b7146b4036
smzht <smzhts1@gmail.com>
2020-04-20 08:57:13 +0900

    コードの見直し

--- config.py
+++ config.py
@@ -1954,15 +1954,11 @@ def configure(keymap):
         # IME 切り替え用のキーの置き換え
         # （Emacs 側での C-F1 と C-F2 の設定については、次のページを参照してください。
         #   https://w.atwiki.jp/ntemacs/pages/48.html ）
-        define_key(keymap_real_emacs, "(243)",  self_insert_command("C-Yen"))
-        define_key(keymap_real_emacs, "(244)",  self_insert_command("C-Yen"))
-        define_key(keymap_real_emacs, "A-(25)", self_insert_command("C-Yen"))
+        keymap_real_emacs["(243)"]  = keymap.InputKeyCommand("C-Yen") # [半角／全角] キー
+        keymap_real_emacs["(244)"]  = keymap.InputKeyCommand("C-Yen") # [半角／全角] キー
+        keymap_real_emacs["A-(25)"] = keymap.InputKeyCommand("C-Yen") # Alt-` キー
 
-        if toggle_input_method_key:
-            for key in toggle_input_method_key:
-                define_key(keymap_real_emacs, key, self_insert_command("C-Yen"))
-
-        if set_input_method_key:
-            for disable_key, enable_key in set_input_method_key:
-                define_key(keymap_real_emacs, disable_key, self_insert_command("C-F1"))
-                define_key(keymap_real_emacs, enable_key,  self_insert_command("C-F2"))
+        keymap_real_emacs["(29)"]   = keymap.InputKeyCommand("C-F1") # [無変換] キー
+        keymap_real_emacs["(28)"]   = keymap.InputKeyCommand("C-F2") # [変換] キー
+        # keymap_real_emacs["O-LAlt"] = keymap.InputKeyCommand("C-F1") # 左 Alt キーのワンショットモディファイア
+        # keymap_real_emacs["O-RAlt"] = keymap.InputKeyCommand("C-F2") # 右 Alt キーのワンショットモディファイア
