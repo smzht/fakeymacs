74602572d70f05c08f95f041b6b21cada6377114
smzht <smzhts1@gmail.com>
2020-10-29 12:35:26 +0900

    拡張機能の配置階層の変更
    shell_command_on_region を実装するにあたり、Extension が複数ファイルに
    なるため、Extension 用のサブフォルダを作成し、管理することとした。

--- _config_personal.py
+++ _config_personal.py
@@ -245,10 +245,10 @@ fc.lancherList_listers = [
 # [section-extensions] -----------------------------------------------------------------------------
 
 # C-Enter に F2（編集モード移行）を割り当てる
-# exec(readConfigExtension("extension-edit_mode.py"), dict(globals(), **locals()))
+# exec(readConfigExtension(r"edit_mode\config.py"), dict(globals(), **locals()))
 
 # Emacs の場合、IME 切り替え用のキーを C-\ に置き換える
-# exec(readConfigExtension("extension-real_emacs.py"), dict(globals(), **locals()))
+# exec(readConfigExtension(r"real_emacs\config.py"), dict(globals(), **locals()))
 
 # 英語キーボード設定をした OS 上で、日本語キーボードを利用する場合の切り替えを行う
-# exec(readConfigExtension("extension-change_keyboard.py"), dict(globals(), **locals()))
+# exec(readConfigExtension(r"change_keyboard\config.py"), dict(globals(), **locals()))
