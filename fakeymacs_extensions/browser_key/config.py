80fb10982fe09eb21b0546415448e3daeb872747
smzht <smzhts1@gmail.com>
2020-11-19 19:31:23 +0900

    登録が漏れていた拡張機能を追加他

---
+++ fakeymacs_extensions/browser_key/config.py
@@ -0,0 +1,30 @@
+﻿# -*- mode: python; coding: utf-8-with-signature-dos -*-
+
+####################################################################################################
+## ブラウザ向けのキー C-l、C-t を発行した際、IME を disable する処理を追加する
+####################################################################################################
+
+try:
+    # 設定されているか？
+    fc.browser_list
+except:
+    fc.browser_list= ["chrome.exe",
+                      "msedge.exe"]
+
+def is_browser(window):
+    if keymap.getWindow().getProcessName() in fc.browser_list:
+        return True
+    else:
+        return False
+
+keymap_browser = keymap.defineWindowKeymap(check_func=is_browser)
+
+def self_insert_command3(*keys):
+    func = self_insert_command(*keys)
+    def _func():
+        func()
+        disable_input_method()
+    return _func
+
+define_key(keymap_browser, "C-l", self_insert_command3("C-l"))
+define_key(keymap_browser, "C-t", self_insert_command3("C-t"))
