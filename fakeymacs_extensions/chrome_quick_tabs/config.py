5938eb4a2d9ef8db368a5c46679b87388531ed0c
smzht <smzhts1@gmail.com>
2020-11-22 20:40:49 +0900

    拡張機能の追加と構成の見直し

---
+++ fakeymacs_extensions/chrome_quick_tabs/config.py
@@ -0,0 +1,21 @@
+﻿# -*- mode: python; coding: utf-8-with-signature-dos -*-
+
+####################################################################################################
+## chrome 系ブラウザで Ctrl-x C-b を入力した際、chrome の機能拡張 quick tabs を起動する
+####################################################################################################
+
+try:
+    # 設定されているか？
+    fc.browser_list
+except:
+    fc.browser_list= ["chrome.exe",
+                      "msedge.exe"]
+
+def list_buffers(func=list_buffers):
+    if (checkWindow("chrome.exe", "Chrome_WidgetWin_1") or # Chrome
+        checkWindow("msedge.exe", "Chrome_WidgetWin_1")):  # MSEode
+        self_insert_command3("C-e")()
+    else:
+        func()
+
+define_key(keymap_emacs, "Ctl-x C-b", reset_search(reset_undo(reset_counter(reset_mark(list_buffers)))))
---
+++ fakeymacs_extensions/vscode_extensions/config.py
@@ -0,0 +1,61 @@
+﻿# -*- mode: python; coding: utf-8-with-signature-dos -*-
+
+####################################################################################################
+## VSCode で Extension のインストールが必要な機能の設定を行う
+####################################################################################################
+
+try:
+    # 設定されているか？
+    fc.vscode_dired
+except:
+    fc.vscode_dired = False
+
+try:
+    # 設定されているか？
+    fc.fc.vscode_recenter
+except:
+    fc.vscode_recenter = False
+
+try:
+    # 設定されているか？
+    fc.vscode_occur
+except:
+    fc.vscode_occur = False
+
+if fc.vscode_dired:
+    # VSCode に vscode-dired Extension をインストールしてお使いください
+    # （Ctrl+x f に設定されているキーバインドは、Ctrl+x（Cut）の機能とバッティングするので、削除して
+    #   ください（Open Keyboard Shortcuts コマンドで削除可能です）)
+
+    def dired(func=dired):
+        if checkWindow("Code.exe", "Chrome_WidgetWin_1"): # VSCode
+            # VSCode Command : Open dired buffer
+            vscodeExecuteCommand("Op-di-bu")
+        else:
+            func()
+
+    define_key(keymap_emacs, "Ctl-x d", reset_search(reset_undo(reset_counter(reset_mark(dired)))))
+
+if fc.vscode_recenter:
+    # VSCode に Center Editor Window Extension をインストールしてお使いください
+
+    def recenter(func=recenter):
+        if checkWindow("Code.exe", "Chrome_WidgetWin_1"): # VSCode
+            # VSCode Command : Center Editor Window
+            self_insert_command("C-l")()
+        else:
+            func()
+
+    define_key(keymap_emacs, "C-l", reset_search(reset_undo(reset_counter(recenter))))
+
+if fc.vscode_occur:
+    # VSCode に Search in Current File Extension をインストールしてお使いください
+    # （アクティビティバーの SEARCH アイコンをパネルのバーにドラッグで持っていくと、検索結果が
+    #   パネルに表示されるようになり、使いやすくなります）
+
+    def occur():
+        if checkWindow("Code.exe", "Chrome_WidgetWin_1"): # VSCode
+            # VSCode Command : Search in Current File
+            vscodeExecuteCommand("Se-in-Cu-Fi")
+
+    define_key(keymap_emacs, "Ctl-x C-o", reset_search(reset_undo(reset_counter(reset_mark(occur)))))
