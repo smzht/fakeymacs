f65a4a312c5f073863cc2f6d56c196439388af72
smzht <smzhts1@gmail.com>
2020-11-28 18:27:26 +0900

    ブラウザ向けのキーの設定の機能強化

--- fakeymacs_extensions/browser_key/config.py
+++ fakeymacs_extensions/browser_key/config.py
@@ -1,7 +1,7 @@
 ﻿# -*- mode: python; coding: utf-8-with-signature-dos -*-
 
 ####################################################################################################
-## ブラウザ向けのキー C-l、C-t を入力した際、IME を disable する処理を追加する
+## ブラウザ向けのキーの設定を行う
 ####################################################################################################
 
 try:
@@ -13,6 +13,8 @@ except:
                       "msedge.exe",
                       "firefox.exe"]
 
+## ブラウザ向けのキー C-l、C-t を入力した際、IME を disable する処理を追加する
+
 def is_browser(window):
     if keymap.getWindow().getProcessName() in fc.browser_list:
         return True
@@ -23,3 +25,23 @@ keymap_browser = keymap.defineWindowKeymap(check_func=is_browser)
 
 define_key(keymap_browser, "C-l", self_insert_command3("C-l"))
 define_key(keymap_browser, "C-t", self_insert_command3("C-t"))
+
+## C-A-l、C-A-t を入力した際、ブラウザをポップアップしてから C-l、C-t の機能を実行する
+## （ブラウザは事前に起動しておいてください）
+
+def browser_popup(key):
+    def _func():
+        for window in getWindowList():
+            if window.getProcessName() in fc.browser_list:
+                popWindow(window)()
+                keymap.delayedCall(self_insert_command3(key), 100)
+                return
+
+        # fc.browser_list に定義するブラウザが起動していない場合、fc.browser_listの最初
+        # に定義するブラウザを起動する
+        keymap.ShellExecuteCommand(None, fc.browser_list[0], "", "")()
+
+    return _func
+
+define_key(keymap_global, "C-A-l", browser_popup("C-l"))
+define_key(keymap_global, "C-A-t", browser_popup("C-t"))
