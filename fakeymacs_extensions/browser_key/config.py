113798b1cb4f2dceacf3626a76431f46f0a8f9fb
smzht <smzhts1@gmail.com>
2020-12-21 18:12:13 +0900

    C-A-e は VSCode 用のキーとして定義済みだったため非採用とする

--- fakeymacs_extensions/browser_key/config.py
+++ fakeymacs_extensions/browser_key/config.py
@@ -13,16 +13,16 @@ except:
                       "msedge.exe",
                       "firefox.exe"]
 
-## C-A-e、C-A-l、C-A-t を入力した際、ブラウザをポップアップしてから C-e、C-l、C-t の機能を実行する
+## C-A-l、C-A-t、C-A-k を入力した際、ブラウザをポップアップしてから C-l、C-t、C-k の機能を実行する
 ## また、IME を指定した状態に変更します。
 
-def browser_popup(key, ime_status):
+def browser_popup(key):
     def _func():
         for window in getWindowList():
             if window.getProcessName() in fc.browser_list:
                 popWindow(window)()
                 self_insert_command(key)()
-                keymap.delayedCall(lambda: keymap.getWindow().setImeStatus(ime_status), 100)
+                keymap.delayedCall(lambda: keymap.getWindow().setImeStatus(0), 100)
                 return
 
         # fc.browser_list に定義するブラウザが起動していない場合、fc.browser_list の最初
@@ -31,7 +31,6 @@ def browser_popup(key, ime_status):
 
     return _func
 
-define_key(keymap_global, "C-A-l", browser_popup("C-l", 0))
-define_key(keymap_global, "C-A-t", browser_popup("C-t", 0))
-define_key(keymap_global, "C-A-e", browser_popup("C-e", 0))
-define_key(keymap_global, "C-A-k", browser_popup("C-k", 1))
+define_key(keymap_global, "C-A-l", browser_popup("C-l"))
+define_key(keymap_global, "C-A-t", browser_popup("C-t"))
+define_key(keymap_global, "C-A-k", browser_popup("C-k"))
