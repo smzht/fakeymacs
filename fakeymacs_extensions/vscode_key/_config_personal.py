e066f1eefdeed76946029192fb16b8d780bf66f6
smzht <smzhts1@gmail.com>
2021-06-26 06:21:11 +0900

    サンプルコードを全面的に見直し

--- fakeymacs_extensions/vscode_key/_config_personal.py
+++ fakeymacs_extensions/vscode_key/_config_personal.py
@@ -5,41 +5,41 @@
 
 # --------------------------------------------------------------------------------------------------
 
-def toggle_editor_layout():
+def rotate_layout():
     # VSCode Command : Toggle Vertical/Horizontal Editor Layout
     self_insert_command("A-S-0")()
     # vscodeExecuteCommand("workbench.action.toggleEditorGroupLayout")()
 
-define_key3(keymap_emacs, "Ctl-x 4", toggle_editor_layout)
+define_key3(keymap_emacs, "Ctl-x 4", rotate_layout)
 
 # --------------------------------------------------------------------------------------------------
 
-def open_folder():
+def find_directory():
     # VSCode Command : File: Open Folder...
     self_insert_command("C-k", "C-o")()
     # vscodeExecuteCommand("workbench.action.files.openFolder")()
 
 # 本設定を行わなくとも、C-A-k C-o を利用することで対応は可能
-define_key3(keymap_emacs, "Ctl-x C-o", reset_search(reset_undo(reset_counter(reset_mark(open_folder)))))
+define_key3(keymap_emacs, "Ctl-x C-d", reset_search(reset_undo(reset_counter(reset_mark(find_directory)))))
 
 # --------------------------------------------------------------------------------------------------
 
-def open_recent():
+def recentf():
     # VSCode Command : File: Open Recent...
     self_insert_command("C-r")()
     # vscodeExecuteCommand("workbench.action.openRecent")()
 
 # 本設定を行わなくとも、C-q C-r を利用することで対応は可能
-define_key3(keymap_emacs, "Ctl-x C-r", reset_search(reset_undo(reset_counter(reset_mark(open_recent)))))
+define_key3(keymap_emacs, "Ctl-x C-r", reset_search(reset_undo(reset_counter(reset_mark(recentf)))))
 
 # --------------------------------------------------------------------------------------------------
 
-def quick_open():
+def locate():
     # VSCode Command : Go to File...
     self_insert_command("C-p")()
     # vscodeExecuteCommand("workbench.action.quickOpen")()
 
 # 本設定を行わなくとも、C-q C-p を利用することで対応は可能
-define_key3(keymap_emacs, "Ctl-x C-p", reset_search(reset_undo(reset_counter(reset_mark(quick_open)))))
+define_key3(keymap_emacs, "Ctl-x C-l", reset_search(reset_undo(reset_counter(reset_mark(locate)))))
 
 # --------------------------------------------------------------------------------------------------
