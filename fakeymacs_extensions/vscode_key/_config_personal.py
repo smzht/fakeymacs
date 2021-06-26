dcb0e85d57ca9da95dde379a496f5687af97ce2c
smzht <smzhts1@gmail.com>
2021-06-27 08:42:26 +0900

    新たなサンプルコードを追加

--- fakeymacs_extensions/vscode_key/_config_personal.py
+++ fakeymacs_extensions/vscode_key/_config_personal.py
@@ -1,4 +1,16 @@
 ﻿# -*- mode: python; coding: utf-8-with-signature-dos -*-
 
 # 本ファイルは、config_personal.py というファイル名にすることで vscode_key Extension の
-# 機能拡張ファイルとして機能します。
+# 機能拡張ファイルとして機能します。以下はサンプルコードです。
+
+# --------------------------------------------------------------------------------------------------
+
+def toggle_panel():
+    # VSCode Command : View: Tggole Panel
+    self_insert_command("C-j")()
+    # vscodeExecuteCommand("workbench.action.togglePanel")()
+
+# 本設定を行わなくとも、C-q C-j を利用することで対応は可能
+define_key3(keymap_emacs, "Ctl-x C-j", toggle_panel)
+
+# --------------------------------------------------------------------------------------------------
