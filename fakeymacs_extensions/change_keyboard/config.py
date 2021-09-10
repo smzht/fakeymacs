d2e9f1b7cfd45c6f744810b2ea9a026b059afdef
smzht <smzhts1@gmail.com>
2021-09-10 17:38:19 +0900

    <¥> キーにスキャンコードを割り当てる方針に見直し

--- fakeymacs_extensions/change_keyboard/config.py
+++ fakeymacs_extensions/change_keyboard/config.py
@@ -20,13 +20,13 @@ except:
     # fc.change_keyboard_startup = "JP"
 
 # OS の設定を英語キーボードにして日本語キーボードを利用する場合のお勧め設定
-# （日本語キーボードの <変換> キーを利用したい場合には、予め、Change Key を使って
-#   日本語キーボードの <変換> キーにスキャンコード 0x7F を割り当ててください。）
+# （予め Change Key を使って、日本語キーボードの <¥> キーにスキャンコード 0x6F を
+#   <変換> キーにスキャンコード 0x7F を割り当ててください。）
 
 keymap.replaceKey(235, 29)               # <無変換> キーを OS が認識可能なキーにする
 keymap.replaceKey(236, 28)               # <変換> キーを OS が認識可能なキーにする
 keymap.replaceKey(193, "RShift")         # <＼> キーを RShift キーにする
-keymap.replaceKey(255, "BackSlash")      # <￥> キーを BackSlash キーにする
+keymap.replaceKey(237, "BackSlash")      # <￥> キーを BackSlash キーにする
 keymap.replaceKey("BackSlash", "Return") # <］> キーを Enter キーにする
 
 # リモートデスクトップで接続する場合など、一つの OS を英語キーボードと日本語キーボード
