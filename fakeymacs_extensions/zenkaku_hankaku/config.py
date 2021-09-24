65d2a55ab0f185c86aaa5b587e7135189a8c9a71
smzht <smzhts1@gmail.com>
2021-09-24 12:02:24 +0900

    hankaku_henkan 拡張機能 を zenkaku_hankaku に名称変更し、機能を拡充

---
+++ fakeymacs_extensions/zenkaku_hankaku/config.py
@@ -0,0 +1,50 @@
+﻿# -*- mode: python; coding: utf-8-with-signature-dos -*-
+
+####################################################################################################
+## 半角と全角の入力を間違えた際、入力モードの切り替えと入力文字の変換を行う（Emacs日本語入力モード用）
+####################################################################################################
+
+try:
+    # 設定されているか？
+    fc.zenkaku_hankaku_key
+except:
+    # 本機能を利用するためのキーを指定する
+    fc.zenkaku_hankaku_key = "C-Enter"
+
+if fc.use_emacs_ime_mode:
+
+    def hankaku_henkan():
+        # 半角英数字に変換し、確定する
+        self_insert_command("F10", "Enter")()
+
+        # IME を OFF にする
+        ei_disable_input_method()
+
+    def zenkaku_henkan():
+        # カーソルの前の word を選択し、クリップボードに格納する
+        mark2(backward_word, False)()
+        copyRegion()
+        delay()
+
+        clipboard_text = getClipboardText()
+
+        # 半角英数字か？（特殊文字は key への変換が難しいので対象外とする）
+        if clipboard_text.encode('utf-8').isalnum():
+
+            # 選択されているリージョンを削除し、IME を ON にする
+            self_insert_command("Delete")()
+            enable_input_method()
+
+            # クリックボードに格納されている英数字を一文字ずつ key として入力する
+            for key in clipboard_text:
+                keymap.InputKeyCommand(key)()
+
+            # Emacs日本語入力モードを ON にする
+            enable_emacs_ime_mode()
+        else:
+            # リージョンを解除し、カーソルの位置を元に戻す
+            resetRegion()
+            forward_word()
+
+    define_key(keymap_ei,    fc.zenkaku_hankaku_key, hankaku_henkan)
+    define_key(keymap_emacs, fc.zenkaku_hankaku_key, reset_undo(reset_counter(reset_mark(zenkaku_henkan))))
