ef52b2cb6af13e7d6490c2bd09b3a4a60bd023db
smzht <smzhts1@gmail.com>
2021-10-01 09:19:48 +0900

    変数の利用方法を見直し

--- fakeymacs_extensions/vscode_key/config.py
+++ fakeymacs_extensions/vscode_key/config.py
@@ -390,15 +390,17 @@ define_key(keymap_vscode, "M-",     keymap.defineMultiStrokeKeymap("Esc"))
 define_key(keymap_vscode, "M-g",    keymap.defineMultiStrokeKeymap("M-g"))
 define_key(keymap_vscode, "M-g M-", keymap.defineMultiStrokeKeymap("M-g Esc"))
 
-fc.vscode_kemap_merged = False
+vscode_kemap_merged = False
 
 def mergeEmacsMultiStrokeKeymap():
-    if not fc.vscode_kemap_merged:
+    global vscode_kemap_merged
+
+    if not vscode_kemap_merged:
         mergeMultiStrokeKeymap(keymap_vscode, keymap_emacs, "Ctl-x")
         mergeMultiStrokeKeymap(keymap_vscode, keymap_emacs, "M-")
         mergeMultiStrokeKeymap(keymap_vscode, keymap_emacs, "M-g")
         mergeMultiStrokeKeymap(keymap_vscode, keymap_emacs, "M-g M-")
-        fc.vscode_kemap_merged = True
+        vscode_kemap_merged = True
 
 ## keymap_emacs キーマップのマルチストロークキーの設定を keymap_vscode キーマップに複写する
 keymap_vscode.applying_func = mergeEmacsMultiStrokeKeymap
