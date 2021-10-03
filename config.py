e3d15cd64e423650c910bfb5dff2fa1aa70b88e4
smzht <smzhts1@gmail.com>
2021-10-03 12:17:52 +0900

    コードの見直し（機能に変更なし）

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 ## Windows の操作を Emacs のキーバインドで行うための設定（Keyhac版）
 ##
 
-fakeymacs_version = "20211003_01"
+fakeymacs_version = "20211003_02"
 
 # このスクリプトは、Keyhac for Windows ver 1.82 以降で動作します。
 #   https://sites.google.com/site/craftware/keyhac-ja
@@ -1382,7 +1382,7 @@ def configure(keymap):
             except:
                 pass
 
-            if isinstance(command, types.FunctionType):
+            if callable(command):
                 if (key is not None and
                     "keymap_emacs" in locals() and
                     window_keymap == locals()["keymap_emacs"]):
