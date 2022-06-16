36d5bdaf2c5e24e8b5c1fab48c48193946677b98
smzht <smzhts1@gmail.com>
2022-06-16 12:23:07 +0900

    OS が英語キーボード設定の時のみ動作するように見直し（機能に変更なし）

--- fakeymacs_extensions/change_keyboard/config.py
+++ fakeymacs_extensions/change_keyboard/config.py
@@ -19,48 +19,49 @@ except:
     fc.change_keyboard_startup = "US"
     # fc.change_keyboard_startup = "JP"
 
-# OS の設定を英語キーボードにして日本語キーボードを利用する場合のお勧め設定
-# （予め Change Key を使って、日本語キーボードの <￥> キーにスキャンコード 0x6F を
-#   <変換> キーにスキャンコード 0x7F を割り当ててください。）
+if not is_japanese_keyboard:
+    # OS の設定を英語キーボードにして日本語キーボードを利用する場合のお勧め設定
+    # （予め Change Key を使って、日本語キーボードの <￥> キーにスキャンコード 0x6F を
+    #   <変換> キーにスキャンコード 0x7F を割り当ててください。）
 
-keymap.replaceKey(235, 29)               # <無変換> キーを OS が認識可能なキーにする
-keymap.replaceKey(236, 28)               # <変換> キーを OS が認識可能なキーにする
-keymap.replaceKey(193, "RShift")         # <＼> キーを RShift キーにする
-keymap.replaceKey(237, "BackSlash")      # <￥> キーを BackSlash キーにする
-keymap.replaceKey("BackSlash", "Return") # <］> キーを Enter キーにする
+    keymap.replaceKey(235, 29)               # <無変換> キーを OS が認識可能なキーにする
+    keymap.replaceKey(236, 28)               # <変換> キーを OS が認識可能なキーにする
+    keymap.replaceKey(193, "RShift")         # <＼> キーを RShift キーにする
+    keymap.replaceKey(237, "BackSlash")      # <￥> キーを BackSlash キーにする
+    keymap.replaceKey("BackSlash", "Return") # <］> キーを Enter キーにする
 
-# リモートデスクトップで接続する場合など、一つの OS を英語キーボードと日本語キーボード
-# とで混在して利用する場合の切り替えの設定
+    # リモートデスクトップで接続する場合など、一つの OS を英語キーボードと日本語キーボード
+    # とで混在して利用する場合の切り替えの設定
 
-def change_keyboard(popBalloon=True):
-    global change_keyboard_status
+    def change_keyboard(popBalloon=True):
+        global change_keyboard_status
 
-    if change_keyboard_status == "US":
-        # 日本語キーボードの利用に切り替える
+        if change_keyboard_status == "US":
+            # 日本語キーボードの利用に切り替える
 
-        # 日本語キーボードの <］> キーを Enter キーにする
-        keymap.replaceKey("BackSlash", "Return")
+            # 日本語キーボードの <］> キーを Enter キーにする
+            keymap.replaceKey("BackSlash", "Return")
 
-        if popBalloon:
-            keymap.popBalloon("keyboard", "[JP Keyboard]", 1000)
+            if popBalloon:
+                keymap.popBalloon("keyboard", "[JP Keyboard]", 1000)
 
+            change_keyboard_status = "JP"
+        else:
+            # 英語キーボードの利用に切り替える
+
+            # 日本語キーボードの <］> キーを元の設定に戻す
+            keymap.replaceKey("BackSlash", "BackSlash")
+
+            if popBalloon:
+                keymap.popBalloon("keyboard", "[US Keyboard]", 1000)
+
+            change_keyboard_status = "US"
+
+    if fc.change_keyboard_startup == "US":
         change_keyboard_status = "JP"
     else:
-        # 英語キーボードの利用に切り替える
-
-        # 日本語キーボードの <］> キーを元の設定に戻す
-        keymap.replaceKey("BackSlash", "BackSlash")
-
-        if popBalloon:
-            keymap.popBalloon("keyboard", "[US Keyboard]", 1000)
-
         change_keyboard_status = "US"
 
-if fc.change_keyboard_startup == "US":
-    change_keyboard_status = "JP"
-else:
-    change_keyboard_status = "US"
+    change_keyboard(False)
 
-change_keyboard(False)
-
-define_key(keymap_global, fc.change_keyboard_key, change_keyboard)
+    define_key(keymap_global, fc.change_keyboard_key, change_keyboard)
