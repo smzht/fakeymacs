7db562e27c5a88feb52279d55f3d6c17400b0efd
smzht <smzhts1@gmail.com>
2022-12-07 19:59:15 +0900

    W-RS-s や W-RS-m が正常に動作しないことの対策

--- config.py
+++ config.py
@@ -254,13 +254,13 @@ def configure(keymap):
     #   のような指定の他に、"M-f" や "Ctl-x d" などの指定も可能です。"M-g*" のようにワイルドカードも
     #   利用することができます。）
     # （ここで指定したキーに新たに別のキー設定をしたいときには、define_key2 関数を利用してください）
-    fc.skip_settings_key    = {"keymap_base"      : ["W-g"], # ベース Keymap
-                               "keymap_global"    : [],      # グローバル Keymap
-                               "keymap_emacs"     : [],      # Emacs キーバインド対象アプリ用 Keymap
-                               "keymap_ime"       : [],      # IME 切り替え専用アプリ用 Keymap
-                               "keymap_ei"        : [],      # Emacs 日本語入力モード用 Keymap
-                               "keymap_tsw"       : [],      # タスク切り替え画面用 Keymap
-                               "keymap_lw"        : [],      # リストウィンドウ用 Keymap
+    fc.skip_settings_key    = {"keymap_base"      : ["W-g", "W-S-*"], # ベース Keymap
+                               "keymap_global"    : [], # グローバル Keymap
+                               "keymap_emacs"     : [], # Emacs キーバインド対象アプリ用 Keymap
+                               "keymap_ime"       : [], # IME 切り替え専用アプリ用 Keymap
+                               "keymap_ei"        : [], # Emacs 日本語入力モード用 Keymap
+                               "keymap_tsw"       : [], # タスク切り替え画面用 Keymap
+                               "keymap_lw"        : [], # リストウィンドウ用 Keymap
                                }
 
     # Emacs のキーバインドにするアプリケーションソフトで、Emacs キーバインドから除外するキーを指定する
