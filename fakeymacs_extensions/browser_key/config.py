b8af56e267ceb40ce0103fc5f953a00e1c704ec4
smzht <smzhts1@gmail.com>
2023-06-01 11:38:57 +0900

    ブラウザの新しいウィンドウを開く機能を追加

--- fakeymacs_extensions/browser_key/config.py
+++ fakeymacs_extensions/browser_key/config.py
@@ -18,23 +18,31 @@ try:
     fc.browser_url
 except:
     # ブラウザが起動していない場合に開く URL を指定する
-    fc.browser_url = r"https://www.google.com/"
+    # fc.browser_url = r"https://www.google.com/"
+    fc.browser_url = r"https://"
 
 try:
     # 設定されているか？
     fc.browser_key1
 except:
-    # アドレスバーに移動するキーを指定する（IME は OFF）
+    # ブラウザをポップアップし、アドレスバーに移動するキーを指定する（IME は OFF）
     fc.browser_key1 = "C-A-l"
 
 try:
     # 設定されているか？
     fc.browser_key2
 except:
-    # 新しいタブを開いてそのタブのアドレスバーに移動するキーを指定する（IME は OFF）
+    # ブラウザをポップアップし、新しいタブを開くキーを指定する（IME は OFF）
     fc.browser_key2 = "C-A-t"
 
-# ブラウザをポップアップしてから指定したキーを実行する。
+try:
+    # 設定されているか？
+    fc.browser_key3
+except:
+    # ブラウザをポップアップし、新しいウィンドウを開くキーを指定する（IME は OFF）
+    fc.browser_key3 = "C-A-o" # C-A-n は VSCode Extension で利用しているため使わない
+
+# ブラウザをポップアップしてから指定したキーを実行する
 def browser_popup(key, ime_status, browser_list=fc.browser_list):
     def _func():
         def _inputKey():
@@ -57,6 +65,7 @@ def browser_popup(key, ime_status, browser_list=fc.browser_list):
 
 define_key(keymap_global, fc.browser_key1, browser_popup("C-l", 0))
 define_key(keymap_global, fc.browser_key2, browser_popup("C-t", 0))
+define_key(keymap_global, fc.browser_key3, browser_popup("C-n", 0))
 
 ## config_personal.py ファイルの読み込み
 exec(readConfigExtension(r"browser_key\config_personal.py", msg=False), dict(globals(), **locals()))
