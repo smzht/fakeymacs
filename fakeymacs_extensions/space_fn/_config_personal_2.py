51a72eb01cfb9bf7ed6106513330c19dc15a2416
smzht <smzhts1@gmail.com>
2023-08-29 18:07:07 +0900

    ファイルの追加漏れ対応

---
+++ fakeymacs_extensions/space_fn/_config_personal_2.py
@@ -0,0 +1,61 @@
+﻿ # -*- mode: python; coding: utf-8-with-signature-dos -*-
+
+# 本ファイルは、config_personal.py というファイル名にすることで space_fn Extension の
+# 機能拡張ファイルとして機能します。以下はサンプルコードです。
+
+# 本サンプルコードは、次のページの設定に準拠しています。
+# https://martin-stone.github.io/touchcursor/help.html
+
+# 本サンプルコードは、キー単体と Shift キーの組み合わせのみのキーの複製を行うバージョンです。
+
+# --------------------------------------------------------------------------------------------------
+
+for window_keymap in fc.space_fn_window_keymap_list:
+
+    # スペースを連続して入力するためのキーの設定
+    replicate_key(window_keymap, "U0-s", "O-Space")
+
+    # キーの複製
+    for key1, key0 in [["j", "Left"],
+                       ["l", "Right"],
+                       ["i", "Up"],
+                       ["k", "Down"],
+                       ["u", "Home"],
+                       ["o", "End"],
+                       ["y", "Insert"],
+                       ["p", "Back"],
+                       ["m", "Delete"],
+                       ["h", "PageUp"],
+                       ["n", "PageDown"]
+                       ]:
+        for mod in ["", "S-"]:
+            mkey0 =         mod + key0
+            mkey1 = "U0-" + mod + key1
+            replicate_key(window_keymap, mkey1, mkey0)
+
+    # ファンクションキーの設定
+    mod = "U0-"
+    for i in range(10):
+        define_key(window_keymap,
+                   mod + f"{(i + 1) % 10}", self_insert_command(mod + vkToStr(VK_F1 + i)))
+
+    define_key(window_keymap, mod + "-", self_insert_command(mod + vkToStr(VK_F11)))
+
+    if is_japanese_keyboard:
+        define_key(window_keymap, mod + "^", self_insert_command(mod + vkToStr(VK_F12)))
+    else:
+        define_key(window_keymap, mod + "=", self_insert_command(mod + vkToStr(VK_F12)))
+
+    mod = "U0-S-"
+    for i in range(10):
+        define_key(window_keymap,
+                   mod + f"{(i + 1) % 10}", self_insert_command(mod + vkToStr(VK_F13 + i)))
+
+    define_key(window_keymap, mod + "-", self_insert_command(mod + vkToStr(VK_F23)))
+
+    if is_japanese_keyboard:
+        define_key(window_keymap, mod + "^", self_insert_command(mod + vkToStr(VK_F24)))
+    else:
+        define_key(window_keymap, mod + "=", self_insert_command(mod + vkToStr(VK_F24)))
+
+# --------------------------------------------------------------------------------------------------
