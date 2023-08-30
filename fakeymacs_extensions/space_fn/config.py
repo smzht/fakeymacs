efd8941a6424990b00e37f1e7852f30036905477
smzht <smzhts1@gmail.com>
2023-08-31 08:44:36 +0900

    US と JIS のキーボード変換の機能を有効にしている場合の対策を追加

--- fakeymacs_extensions/space_fn/config.py
+++ fakeymacs_extensions/space_fn/config.py
@@ -35,6 +35,16 @@ def replicate_key(window_keymap, keys, original_keys):
 
 keymap.defineModifier(fc.space_fn_key, "User0")
 
+# US と JIS のキーボード変換の機能を有効にしている場合は、変換が必要となるキーを keymap_base
+# に登録する
+if use_usjis_keyboard_conversion:
+    for us_key, jis_list in usjis_key_table.items():
+        if jis_list[0]:
+            for mod1, mod2, mod3 in itertools.product(["", "W-"], ["", "A-"], ["", "C-"]):
+                mkey0 =         mod1 + mod2 + mod3 + us_key
+                mkey1 = "U0-" + mod1 + mod2 + mod3 + us_key
+                define_key(keymap_base, mkey1, self_insert_command(mkey0))
+
 mkey0 = fc.space_fn_key
 mkey1 = "O-" + fc.space_fn_key
 
