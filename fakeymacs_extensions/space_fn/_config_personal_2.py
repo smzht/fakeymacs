2f2dfbaebc81ff704533762c7f2ee93251789a74
smzht <smzhts1@gmail.com>
2023-09-04 08:17:17 +0900

    不要なスペース削除

--- fakeymacs_extensions/space_fn/_config_personal_2.py
+++ fakeymacs_extensions/space_fn/_config_personal_2.py
@@ -1,4 +1,4 @@
-﻿ # -*- mode: python; coding: utf-8-with-signature-dos -*-
+﻿# -*- mode: python; coding: utf-8-with-signature-dos -*-
 
 # 本ファイルは、config_personal.py というファイル名にすることで space_fn Extension の
 # 機能拡張ファイルとして機能します。以下はサンプルコードです。
