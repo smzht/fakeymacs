31bbf7df2c876f9f181f7fcc04113a3086e55012
smzht <smzhts1@gmail.com>
2023-09-06 08:34:45 +0900

    性能改善（機能に変更なし）

--- fakeymacs_extensions/window_switching_key/config.py
+++ fakeymacs_extensions/window_switching_key/config.py
@@ -11,8 +11,22 @@ except:
     # ウィンドウを切り替えるキーの組み合わせ（前、後 の順）を指定する（複数指定可）
     fc.window_switching_key2 = [["A-S-p", "A-S-n"]]
 
+window_list = []
+process_name = ""
+unix_time = 0
+
 def windowList():
-    return getWindowList(None, keymap.getWindow().getProcessName())
+    global window_list
+    global process_name
+    global unix_time
+
+    if (process_name != keymap.getWindow().getProcessName() or
+        (time.time() - unix_time > 2)):
+        process_name = keymap.getWindow().getProcessName()
+        window_list = getWindowList(None, process_name)
+
+    unix_time = time.time()
+    return window_list
 
 for previous_key, next_key in fc.window_switching_key2:
     define_key(keymap_global, previous_key, previous_window(windowList))
