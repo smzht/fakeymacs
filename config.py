5f0ed06c085c969fb5d59853367f318ae566a48f
smzht <smzhts1@gmail.com>
2023-09-07 07:26:02 +0900

    パラメータ名称の見直し（機能に変更なし）

--- config.py
+++ config.py
@@ -6,7 +6,7 @@
 ##  Windows の操作を Emacs のキーバインドで行うための設定（Keyhac版）
 #########################################################################
 
-fakeymacs_version = "20230906_06"
+fakeymacs_version = "20230907_01"
 
 import time
 import os.path
@@ -2606,8 +2606,8 @@ def configure(keymap):
         fakeymacs.unix_time = time.time()
         return fakeymacs.window_list2
 
-    def switchWindows(func, direction):
-        window_list = func()
+    def switchWindows(window_list_func, direction):
+        window_list = window_list_func()
 
         # ２つのリストに差異があるか？
         if set(window_list) ^ set(fakeymacs.window_list):
@@ -2619,14 +2619,14 @@ def configure(keymap):
             popWindow(window_list[0])()
             fakeymacs.window_list = window_list
 
-    def previous_window(func):
+    def previous_window(window_list_func):
         def _func():
-            keymap.delayedCall(lambda: switchWindows(func, "previous"), 0)
+            keymap.delayedCall(lambda: switchWindows(window_list_func, "previous"), 0)
         return _func
 
-    def next_window(func):
+    def next_window(window_list_func):
         def _func():
-            keymap.delayedCall(lambda: switchWindows(func, "next"), 0)
+            keymap.delayedCall(lambda: switchWindows(window_list_func, "next"), 0)
         return _func
 
     def move_window_to_previous_display():
