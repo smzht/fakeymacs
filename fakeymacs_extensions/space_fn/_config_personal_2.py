4dc45d7a66c58e01a882547a3961383735c518ff
smzht <smzhts1@gmail.com>
2023-10-26 19:40:11 +0900

    ファンクションキーの設定を _config_personal-1.py と同じ設定に見直し

--- fakeymacs_extensions/space_fn/_config_personal_2.py
+++ fakeymacs_extensions/space_fn/_config_personal_2.py
@@ -7,6 +7,8 @@
 # https://martin-stone.github.io/touchcursor/help.html
 
 # 本サンプルコードは、キー単体と Shift キーの組み合わせのみのキーの複製を行うバージョンです。
+# （ファンクションキーの設定は、_config_personal_1.py と同様、すべてのモディファイアキーと
+#   組合せたキーの複製を行なう設定としています。）
 
 # --------------------------------------------------------------------------------------------------
 
@@ -38,28 +40,33 @@ for window_keymap in fc.space_fn_window_keymap_list:
             replicate_key(window_keymap, mkey1, mkey0)
 
     # ファンクションキーの設定
-    mkey = "U0-"
-    for i in range(10):
-        define_key_fn(window_keymap,
-                      mkey + f"{(i + 1) % 10}", self_insert_command(vkToStr(VK_F1 + i)))
+    for mod1, mod2, mod3 in itertools.product(["", "W-"], ["", "A-"], ["", "C-"]):
+        mod   = mod1 + mod2 + mod3
 
-    define_key_fn(window_keymap, mkey + "-", self_insert_command(vkToStr(VK_F11)))
+        mkey0 =         mod
+        mkey1 = "U0-" + mod
+        for i in range(10):
+            define_key_fn(window_keymap,
+                          mkey1 + f"{(i + 1) % 10}", self_insert_command(mkey0 + vkToStr(VK_F1 + i)))
 
-    if is_japanese_keyboard:
-        define_key_fn(window_keymap, mkey + "^", self_insert_command(vkToStr(VK_F12)))
-    else:
-        define_key_fn(window_keymap, mkey + "=", self_insert_command(vkToStr(VK_F12)))
+        define_key_fn(window_keymap, mkey1 + "-", self_insert_command(mkey0 + vkToStr(VK_F11)))
 
-    mkey = "U0-S-"
-    for i in range(10):
-        define_key_fn(window_keymap,
-                      mkey + f"{(i + 1) % 10}", self_insert_command(vkToStr(VK_F13 + i)))
+        if is_japanese_keyboard:
+            define_key_fn(window_keymap, mkey1 + "^", self_insert_command(mkey0 + vkToStr(VK_F12)))
+        else:
+            define_key_fn(window_keymap, mkey1 + "=", self_insert_command(mkey0 + vkToStr(VK_F12)))
 
-    define_key_fn(window_keymap, mkey + "-", self_insert_command(vkToStr(VK_F23)))
+        mkey0 =           mod
+        mkey1 = "U0-S-" + mod
+        for i in range(10):
+            define_key_fn(window_keymap,
+                          mkey1 + f"{(i + 1) % 10}", self_insert_command(mkey0 + vkToStr(VK_F13 + i)))
 
-    if is_japanese_keyboard:
-        define_key_fn(window_keymap, mkey + "^", self_insert_command(vkToStr(VK_F24)))
-    else:
-        define_key_fn(window_keymap, mkey + "=", self_insert_command(vkToStr(VK_F24)))
+        define_key_fn(window_keymap, mkey1 + "-", self_insert_command(mkey0 + vkToStr(VK_F23)))
+
+        if is_japanese_keyboard:
+            define_key_fn(window_keymap, mkey1 + "^", self_insert_command(mkey0 + vkToStr(VK_F24)))
+        else:
+            define_key_fn(window_keymap, mkey1 + "=", self_insert_command(mkey0 + vkToStr(VK_F24)))
 
 # --------------------------------------------------------------------------------------------------
