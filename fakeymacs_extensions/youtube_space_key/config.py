54fed7bc8ed1753fe96e48c4239f82f65b8c3d12
smzht <smzhts1@gmail.com>
2023-10-29 17:28:36 +0900

    YouTube で Space の機能を正常に動作させるための拡張機能を追加

---
+++ fakeymacs_extensions/youtube_space_key/config.py
@@ -0,0 +1,28 @@
+﻿# -*- mode: python; coding: utf-8-with-signature-dos -*-
+
+####################################################################################################
+## YouTube で Space による停止、再生が正しく機能しないことの暫定的な対策を行う
+####################################################################################################
+
+try:
+    # 設定されているか？
+    fc.youtube_browser_list
+except:
+    # 本対策を行うブラウザのプログラム名称を指定する
+    fc.youtube_browser_list = ["chrome.exe",
+                               "msedge.exe",
+                               "firefox.exe",
+                               ]
+
+def space():
+    if (fc.ime_reconv_key is None or
+        fakeymacs.forward_direction is None):
+        if (keymap.getWindow().getProcessName() in fc.youtube_browser_list and
+            " - YouTube " in keymap.getWindow().getText()):
+            self_insert_command("D-Space")()
+        else:
+            self_insert_command("Space")()
+    else:
+        reconversion()
+
+define_key(keymap_emacs, "Space", reset_undo(reset_counter(reset_mark(repeat(space)))))
