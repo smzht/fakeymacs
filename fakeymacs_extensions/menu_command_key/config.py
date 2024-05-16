22888ab315ed491daf1c289b2bfea8e9d4a46ce3
smzht <smzhts1@gmail.com>
2024-05-16 17:45:51 +0900

    パラメータの見直しとスコープの不具合を対策

--- fakeymacs_extensions/menu_command_key/config.py
+++ fakeymacs_extensions/menu_command_key/config.py
@@ -8,12 +8,11 @@ try:
     # 設定されているか？
     fc.menu_command_key
 except:
-    # 実行するアプリのリスト、メニューのコマンドID、割り当てるキーの組み合わせを指定する（複数指定可）
-    fc.menu_command_key = [[["chrome.exe",
-                             "msedge.exe"], 35024, "C-A-r"], # 現在のタブの右側に新たなタブを開く
+    # 実行するアプリ、メニューのコマンドID、割り当てるキーの組み合わせを指定する（複数指定可）
+    fc.menu_command_key = [["chrome.exe", 35024, "C-A-r"], # 現在のタブの右側に新たなタブを開く
                            ]
 
-for app_list, command_id, key in fc.menu_command_key:
+for process_name, command_id, key in fc.menu_command_key:
     define_key3(keymap_global, key,
-                lambda: keymap.getWindow().postMessage(0x0111, command_id),
-                lambda: keymap.getWindow().getProcessName() in app_list)
+                lambda x=command_id:  keymap.getWindow().postMessage(0x0111, x),
+                lambda x=process_name:keymap.getWindow().getProcessName() == x)
