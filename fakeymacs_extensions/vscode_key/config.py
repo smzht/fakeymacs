887f7e05522242d009748fe648eca7cfede3080e
smzht <smzhts1@gmail.com>
2025-04-24 10:07:51 +0900

    ターミナルの判定を少し緩くする

--- fakeymacs_extensions/vscode_key/config.py
+++ fakeymacs_extensions/vscode_key/config.py
@@ -258,8 +258,8 @@ def post(func):
             fakeymacs_vscode.post_processing = None
     return _func
 
-pattern1 = re.compile("|".join([rf" - {v}$" for v in ["Terminal", "ターミナル"]]))
-pattern2 = re.compile("|".join([rf"^{t} .* -$" for t in fc.terminal_list_for_direct_input]))
+pattern1 = re.compile("|".join([rf" - {v}" for v in ["Terminal", "ターミナル"]]))
+pattern2 = re.compile("|".join([rf"^{t} - " for t in fc.terminal_list_for_direct_input]))
 
 def is_terminal_for_direct_input():
     title = keymap.getWindow().getText()
