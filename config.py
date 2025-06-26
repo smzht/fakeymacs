a499c988c5ff26fc576f255a039042386df89775
smzht <smzhts1@gmail.com>
2025-06-26 15:40:20 +0900

    チューンナップ

--- config.py
+++ config.py
@@ -612,7 +612,7 @@ def configure(keymap):
 
             if keymap.hook_enabled:
                 if event == EVENT_SYSTEM_FOREGROUND:
-                    delay(0.5)
+                    delay(0.2)
                     if hwnd == user32.GetForegroundWindow():
                         try:
                             window = Window.getFocus()
