0281034a99ad521a8fb45114f6512f76789f00bf
smzht <smzhts1@gmail.com>
2025-08-24 09:13:24 +0900

    文言の見直し

--- fakeymacs_extensions/shell_command_on_region/config.py
+++ fakeymacs_extensions/shell_command_on_region/config.py
@@ -8,14 +8,14 @@ try:
     # 設定されているか？
     fc.foreground_timeout
 except:
-    # フォアグラウンド処理（C-u を付けた場合）のタイムアウト値（秒）を設定する
+    # フォアグラウンド処理（C-u を付けた場合）のタイムアウト値（秒）を指定する
     fc.foreground_timeout = 10
 
 try:
     # 設定されているか？
     fc.background_timeout
 except:
-    # バックグラウンド処理（C-u を付けない場合）のタイムアウト値（秒）を設定する
+    # バックグラウンド処理（C-u を付けない場合）のタイムアウト値（秒）を指定する
     fc.background_timeout = 600
 
 try:
