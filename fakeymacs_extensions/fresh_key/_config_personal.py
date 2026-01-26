f60879b1b1ddc599e9ca776451d5540c146447ae
smzht <smzhts1@gmail.com>
2026-01-26 18:02:36 +0900

    サンプル設定を追加

--- fakeymacs_extensions/fresh_key/_config_personal.py
+++ fakeymacs_extensions/fresh_key/_config_personal.py
@@ -5,4 +5,13 @@
 
 # --------------------------------------------------------------------------------------------------
 
+fc.fresh_command_dict["Toggle File Explorer"] = ["tofiex", "エ切"]
+
+def toggle_file_explorer():
+    freshExecuteCommand("Toggle File Explorer")()
+
+define_key_f("Ctl-x C-e", reset_search(reset_undo(reset_counter(reset_mark(toggle_file_explorer)))))
+
+# ファイルエクスポーラにフォーカスを当てる（C-e）機能は、C-q C-e で OK
+
 # --------------------------------------------------------------------------------------------------
