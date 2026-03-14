f1bcec94b3b00148b4ddc8aae1a7fadae50c1330
smzht <smzhts1@gmail.com>
2026-03-14 22:44:03 +0900

    追加登録

---
+++ fakeymacs_extensions/vim_key/init.lua
@@ -0,0 +1,7 @@
+vim.opt.title = true
+vim.opt.hidden = true
+vim.opt.number = true
+vim.opt.selection = 'exclusive'
+vim.opt.virtualedit = 'onemore'
+vim.opt.whichwrap = 'b', 's', 'h', 'l', '<', '>', '[', ']'
+vim.opt.incsearch = true
