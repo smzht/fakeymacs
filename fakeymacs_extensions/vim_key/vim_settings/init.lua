8cc82cbe3ea416e47dca2dfea0f650458a9d5d5e
smzht <smzhts1@gmail.com>
2026-04-19 01:42:09 +0900

    nvim のタイトル判定文字列の追加と nvim終了時のターミナルタイトル初期化の対策

--- fakeymacs_extensions/vim_key/vim_settings/init.lua
+++ fakeymacs_extensions/vim_key/vim_settings/init.lua
@@ -1,10 +1,16 @@
 vim.opt.title = true
 vim.opt.hidden = true
 vim.opt.number = true
-vim.opt.ambiwidth = 'double'
-vim.opt.selection = 'exclusive'
-vim.opt.virtualedit = 'onemore'
-vim.opt.whichwrap = 'b', 's', 'h', 'l', '<', '>', '[', ']'
+vim.opt.ambiwidth = "double"
+vim.opt.selection = "exclusive"
+vim.opt.virtualedit = "onemore"
+vim.opt.whichwrap = "b", "s", "h", "l", "<", ">", "[", "]"
 vim.opt.incsearch = true
 
-vim.keymap.set('i', '<C-j>', '<C-o>')
+vim.keymap.set("i", "<C-j>", "<C-o>"
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    os.execute("echo -ne '\\033]0;\\007'")
+  end,
+}))
