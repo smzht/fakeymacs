a7836605c9fd97d9f84ef1b7bb9a6890a4d5f9c5
smzht <smzhts1@gmail.com>
2026-04-20 09:23:13 +0900

    設定誤り訂正

--- fakeymacs_extensions/vim_key/vim_settings/init.lua
+++ fakeymacs_extensions/vim_key/vim_settings/init.lua
@@ -7,10 +7,10 @@ vim.opt.virtualedit = "onemore"
 vim.opt.whichwrap = "b", "s", "h", "l", "<", ">", "[", "]"
 vim.opt.incsearch = true
 
-vim.keymap.set("i", "<C-j>", "<C-o>"
+vim.keymap.set("i", "<C-j>", "<C-o>")
 
 vim.api.nvim_create_autocmd("VimLeave", {
   callback = function()
     os.execute("echo -ne '\\033]0;\\007'")
   end,
-}))
+})
